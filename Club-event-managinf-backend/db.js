b1d4fbbd40a29c767600efded58ac4908d719e7a
Samhitha1212 <samhithadondeti@gmail.com>
2024-06-14 19:28:16 +0530

    connected to database

--- Club-event-managinf-backend/db.js
+++ Club-event-managinf-backend/db.js
@@ -1,22 +1,6 @@
-const {MongoClient} =require('mongodb')
+const mongoose =require('mongoose')
+const url='mongodb+srv://Samhitha1212:Samhitha%40121@cluster0.yn8m78o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
-let dbConnection;
-
-module.exports={
-  connectToDb:(cb)=>{
-    MongoClient.connect('')
-    .then((client)=>{
-      dbConnection=client.db()
-      return cb()
-    })
-    .catch(err=>{
-      console.log(err)
-      return cb(err)
-    })
-
-  },
-  getDb:()=>{
-    return dbConnection
-  }
-
+module.exports.connect=()=>{
+  mongoose.connect(url,console.log("Data base is connected"))
 }
--- Club-event-managinf-backend/index.js
+++ Club-event-managinf-backend/index.js
@@ -1,20 +1,20 @@
 const express=require('express')
 const app=express()
 const cors=require('cors')
+const EventRoutes=require('./Routes/EventRoutes.js')
 
-const {connectToDb,getDb} =require('./db')
+const {connect} =require('./db.js')
 
 app.use(cors())
 app.use(express.json())
+app.use("/api/event",EventRoutes)
+connect()
 
-connectToDb(()=>{
-  console.log("connection establised")
+
+app.get('/',(req,res)=>{
+  res.send("this is home page")
 })
 
-
-app.post('/',(req,res)=>{
-  console.log(req.body)
-})
 
 app.listen(3000,()=>{
   console.log('listening to port 3000')
